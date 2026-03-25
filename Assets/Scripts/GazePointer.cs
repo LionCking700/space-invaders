1ad6af81219a2a2f09d494515453b46b7e228fe0
YesmeAlba <Hellckingder2000@gmail.com>
2026-03-24 20:39:02 -0600

    spaceInvaders003

--- Assets/Scripts/GazePointer.cs
+++ Assets/Scripts/GazePointer.cs
@@ -1,38 +1,66 @@
-using UnityEngine;
 using System.Collections.Generic;
-using UnityEngine.EventSystem;
-using UnityEngine.UI;
-
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.EventSystems;
+ 
 public class GazePointer : MonoBehaviour
 {
-[SerializeFiled]
-private LayerMask interactableLayer;
-private EventSystem eventSystem;
-private PointerEventData pointerEventData;
-private GameObject currentObjct;
-private void Start()
+    [SerializeField]
+ 
+    private LayerMask interactableLayer;
+ 
+    private EventSystem eventSystem;
+ 
+    private PointerEventData pointerEventData;
+ 
+    private GameObject currentObject;
+ 
+    private void Start()
     {
         eventSystem = EventSystem.current;
         if (eventSystem == null)
         {
             GameObject eventSystemObj = new GameObject("EventSystem");
-            eventSystem = eventSystemObj.AddComponent<eventSystem>();
+            eventSystem = eventSystemObj.AddComponent<EventSystem>();
             eventSystemObj.AddComponent<StandaloneInputModule>();
         }
         pointerEventData = new PointerEventData(eventSystem);
     }
-    private void Update();
+    private void Update()
     {
-        pointerEventData.position = new Vector2(Screen.Width / 2, Screen.height / 2);
-        List<RaycastResult> raycastResults = new list<RaycastResult>();
+        pointerEventData.position = new Vector2(Screen.width / 2, Screen.height / 2);
+        List<RaycastResult> raycastResults = new List<RaycastResult>();
         eventSystem.RaycastAll(pointerEventData, raycastResults);
         GameObject hitObject = GetFirstValid(raycastResults);
         if (currentObject != hitObject)
         {
             if (currentObject != null)
             {
-                
+                currentObject.SendMessage("OnPointExit", pointerEventData, SendMessageOptions.DontRequireReceiver);
             }
+            if (hitObject != null)
+            {
+                hitObject.SendMessage("OnPointEnter", pointerEventData, SendMessageOptions.DontRequireReceiver);
+            }
+            currentObject = hitObject;
+        }
+        if (Input.GetMouseButton(0) && currentObject != null)
+        {
+            currentObject.SendMessage("OnPointerClick", pointerEventData, SendMessageOptions.DontRequireReceiver);
         }
     }
+ 
+    GameObject GetFirstValid(List<RaycastResult> results)
+    {
+        foreach (RaycastResult result in results)
+        {
+            if (((1 << result.gameObject.layer)& interactableLayer) !=0)
+            {
+                return result.gameObject;
+            }
+ 
+        }
+        return null;
+    }
+ 
 }
